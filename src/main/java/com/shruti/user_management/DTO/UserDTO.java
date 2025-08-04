087723f9961f9f124906fd1f3d93e453d277767c
Shruti Jena <shruti_jena@apple.com>
2025-08-04 18:50:15 +0530

    Basic spring data jpa project

---
+++ src/main/java/com/shruti/user_management/DTO/UserDTO.java
@@ -0,0 +1,22 @@
+// Package Declaration
+package com.shruti.user_management.DTO;
+
+// Validation Annotations
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+//Lombok to generate boiler plate code
+import lombok.Data;
+
+public class UserDTO {
+
+@NotBlank(message = "Name is required")
+private String name;
+
+@Email(message = "Email should be  valid")
+@NotBlank(message = "Email is required" )
+private String email;
+
+@NotBlank(message = "Passowrd is required")
+private String password;
+}
